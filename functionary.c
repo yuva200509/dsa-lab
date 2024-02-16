c2668e52585b3489ccf0043c5d6ccce707df5316
k yuva sai <160228640+yuva200509@users.noreply.github.com>
2024-02-16 16:44:30 +0530

    Create functionary.c

---
+++ functionary.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+
+void sort(int a[],int n)
+{
+int i,temp,j;
+for(i=0;i<n-1;++i)
+{
+for(j=0;j<n-i-1;++j)
+{
+if(a[j]>a[j+1])
+{
+temp=a[j+1];
+a[j+1]=a[j];
+a[j]=temp;
+}
+}
+}
+}
+int main()
+{
+int mid,end,start=0,i,n,a[50],key;
+printf("enter a size of the array");
+scanf("%d",&n);
+printf("enter a key");
+scanf("%d",&key);
+printf("enter a elements");
+for(i=0;i<n;++i)
+{
+scanf("%d",&a[i]);
+}
+sort(a,n);
+printf("storted array=");
+for(i=0;i<n;++i)
+{
+printf("%d",a[i]);
+}
+end=n-1;
+while(start<=end)
+{
+mid=(start+end)/2;
+if(key==a[mid]){
+printf("\n key found at %d",mid+1);
+break;
+}
+else if(key<a[mid])
+{
+end=mid;
+}
+else if(key>a[mid])
+{
+start=mid;
+}
+else
+{
+printf("elements not found");
+return 0;
+}
+}
+return 0;
+}
